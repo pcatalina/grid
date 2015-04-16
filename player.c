8790a105028ba51ed7d900ce5f4b3fbbe11b0a18
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-16 17:00:57 +0200

    refactor keyboard input, add ncurses

--- player.c
+++ player.c
@@ -1,47 +1,80 @@
+#include <curses.h>
+#include <assert.h>
 #include "player.h"
 
+void bounceChecking()
+{
 
+}
+
+void go_up(player* p, grid* g)
+{
+	p->position.x = p->position.x - 1;
+	g->plan[p->position.x][p->position.y] = 2;
+	print_grid(*g);
+
+}
+
+void go_right(player* p, grid* g)
+{
+	p->position.y = p->position.y + 1;
+	g->plan[p->position.x][p->position.y] = 2;
+	print_grid(*g);
+}
+
+
+void go_down(player* p, grid* g)
+{
+	p->position.x = p->position.x + 1;
+	g->plan[p->position.x][p->position.y] = 2;
+	print_grid(*g);
+}
+
+void go_left(player* p, grid* g)
+{
+	p->position.y = p->position.y - 1;
+	g->plan[p->position.x][p->position.y] = 2;
+	print_grid(*g);
+}
 
 void move_player(char* file_name)
 {
+	WINDOW * mainwin;
+
+	if ((mainwin = initscr()) == NULL) 
+	{
+		fprintf(stderr, "Error initialising ncurses.\n");
+		exit(EXIT_FAILURE);
+	}
+	noecho();
 	player p;
 	p.position = load_point(file_name);
 	grid g = load_grid(file_name);
-	char new_position;
-	printf("Control keys:\nI *up*\nL *right*\nK *down*\nJ *left*\n");
+	
+
+	printf("Control keys:\nw *up*\nd *right*\ns *down*\na *left*\n");
 	int n = 0;
-	while (n < MAX_LIVES)
+
+
+	char move ;
+	print_grid(g);
+	while (move = getch())
 	{
-		scanf("%c", &new_position);
-
-		if ((new_position == 'd') && 
-			(g.plan[p.position.x + 1][p.position.y] != 1))
+		switch (move)
 		{
-			g.plan[p.position.x + 1][p.position.y] = 2;
-
-
-		}
-		else if ((new_position == 's') && (g.plan[p.position.x][p.position.y - 1] != 1))
-		{
-			g.plan[p.position.x][p.position.y - 1] = 2;
-
-
-		}
-		else if ((new_position == 'a') && (g.plan[p.position.x - 1][p.position.y] != 1))
-		{
-			g.plan[(p.position.x) - 1][p.position.y] = 2;
-
-
-		}
-		else if ((new_position == 'w') && (g.plan[p.position.x][p.position.y + 1] != 1))
-		{
-			g.plan[p.position.x][p.position.y + 1] = 2;
-
-
+		case'w': go_up(&p, &g);
+			break;
+		case'd': go_right(&p, &g);
+			break;
+		case's': go_down(&p, &g);
+			break;
+		case 'a': go_left(&p, &g);
+			break;
+		default: break;
 		}
 
-		g.plan[p.position.x][p.position.y] = 0;
-		print_grid(g);
-		n++;
+	g.plan[p.position.x][p.position.y] = 0;
+	refresh();
 	}
+	//getchar();
 }
