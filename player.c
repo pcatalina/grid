1d652fcaaf2910443bf7345fc13ea9b6cddb2b95
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-16 19:02:28 +0200

    refactor movement code

--- player.c
+++ player.c
@@ -2,64 +2,91 @@
 #include <assert.h>
 #include "player.h"
 
-void bounceChecking()
+point get_position(player* p)
 {
-
+	return p->position;
 }
 
+void set_position(player* p, point pt)
+{
+	p->position.x = pt.x;
+	p->position.y = pt.y;
+}
+
+point sum_points(point p, point q)
+{
+	point t;
+	t.x = p.x + q.x;
+	t.y = p.y + q.y;
+	return t;
+}
+
+void go(player* p, grid* g, point direction)
+{
+	point old_position = get_position(p);
+	int previous = get_cell(g, old_position);
+	point new_position = sum_points(old_position, direction);
+	set_cell(g, new_position, 2);
+	set_cell(g, old_position, 0);
+	set_position(p, new_position);
+	print_grid(*g);
+}
 void go_up(player* p, grid* g)
 {
-	p->position.x = p->position.x - 1;
-	g->plan[p->position.x][p->position.y] = 2;
-	print_grid(*g);
+	point position = { -1, 0};
+	go(p, g, position);
 
 }
 
 void go_right(player* p, grid* g)
 {
-	p->position.y = p->position.y + 1;
-	g->plan[p->position.x][p->position.y] = 2;
-	print_grid(*g);
+	point position = { 0, 1 };
+	go(p, g, position);
 }
 
 
 void go_down(player* p, grid* g)
 {
-	p->position.x = p->position.x + 1;
-	g->plan[p->position.x][p->position.y] = 2;
-	print_grid(*g);
+	point position = { 1, 0 };
+	go(p, g, position);
 }
 
 void go_left(player* p, grid* g)
 {
-	p->position.y = p->position.y - 1;
-	g->plan[p->position.x][p->position.y] = 2;
-	print_grid(*g);
+	point position = { 0, -1 };
+	go(p, g, position);
 }
 
 void move_player(char* file_name)
 {
 	WINDOW * mainwin;
 
-	if ((mainwin = initscr()) == NULL) 
+	if ((mainwin = initscr()) == NULL)
 	{
 		fprintf(stderr, "Error initialising ncurses.\n");
 		exit(EXIT_FAILURE);
 	}
 	noecho();
+
+
 	player p;
 	p.position = load_point(file_name);
+	point player_position = p.position;
 	grid g = load_grid(file_name);
-	
-
-	printf("Control keys:\nw *up*\nd *right*\ns *down*\na *left*\n");
-	int n = 0;
 
 
-	char move ;
+	//printw("Control keys:\nw *up*\nd *right*\ns *down*\na *left*\n");
+
+	set_cell(&g, player_position, 2);
 	print_grid(g);
+
+	char move;
+
 	while (move = getch())
 	{
+
+		clear();
+		
 		switch (move)
 		{
 		case'w': go_up(&p, &g);
@@ -73,8 +100,7 @@ void move_player(char* file_name)
 		default: break;
 		}
 
-	g.plan[p.position.x][p.position.y] = 0;
-	refresh();
+		refresh();
 	}
 	//getchar();
 }
