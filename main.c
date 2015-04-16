326a4668ea4f42aac367dea0df21062bbba5a307
pcatalina <catalina.pavlov@etu.unistra.fr>
2015-04-16 16:58:18 +0200

    remove unused functions

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
 #include "grid.h"
 #include "player.h"
 #define MAX_LINE 512
-//#define FILE "grid.txt"
 
 
 int main()
 {
-	grid g = init_grid(7, 9);
-	print_grid(g);
-	point p = { 3, 4 };
-	load_grid("grid.txt");
 	move_player("grid.txt");
+
 	getchar();
 	return 0;
 }
